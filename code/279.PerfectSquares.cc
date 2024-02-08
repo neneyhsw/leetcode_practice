f95a2492b2eaee1770dcddd307784d0350ec34ee
neneyhsw <neneyhsw@gmail.com>
2024-02-08 20:46:30 +0800

    add question 279.

---
+++ code/279.PerfectSquares.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n+1, 0);
+        for (int i = 1; i <= n; i++) {
+          dp[i] = i;
+          for (int j = 1; j * j <= i; j++) {
+            dp[i] = min(dp[i], dp[i - j*j] + 1);
+          }
+        }
+        return dp[n];
+    }
+};
+
+int main(int argc, char *argv[]) {
+  Solution S;
+  int n = 13;
+
+  cout << S.numSquares(n) << endl;
+
+  return 0;
+}
